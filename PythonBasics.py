948266292a11cec080fa6ce1ef1343f714d9886c
Prajj-10 <prajjwalveerbasnet10@gmail.com>
2023-09-28 13:15:59 +0545

    Python Basics program added.

---
+++ PythonBasics.py
@@ -0,0 +1,34 @@
+# Declaration and initialization of a variable
+PI = 3.14  # The identifier's name is in all Caps, hence this must be treated as a constant.
+
+a = 34  # Assigning 34 to a variable called a.
+
+b = 50
+
+# Basic Arithmetic operations
+
+c = a + b  # Addition of numbers
+d = b - a  # Subtraction of numbers
+e = b * a  # Multiplication of numbers
+f = b / a  # Division of numbers
+
+# Further arithmetic operations
+
+g = b ** a  # Exponent of b^a
+h = b // a  # Integer division of b/a (Store the whole number of the quotient)
+i = b % a   # Modulus of b and a (Stores remainder in i.)
+
+# Printing values.
+
+print(c)
+print(d)
+print(e)
+print(f)
+
+print(g)
+print(h)
+print(i)
+
+# Printing in the same line.
+
+print(a, b, end='')
